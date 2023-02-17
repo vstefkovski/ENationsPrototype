9d97ffacc629cc2abcc72bed103bfa60ba58886a
Vojdan Stefkovski <vojdan.stefkovski@students.finki.ukim.mk>
2023-02-17 15:11:19 +0100

    first commit

---
+++ src/main/java/mk/ukim/finki/enationsprototype/ENationsPrototypeApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.enationsprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENationsPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ENationsPrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/enationsprototype/ENationsPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.enationsprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENationsPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
